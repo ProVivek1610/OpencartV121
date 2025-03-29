c31ab64544a2eaa701aa1fc7579bb1db74902863
ProVivek1610 <153586041+ProVivek1610@users.noreply.github.com>
2025-03-29 19:20:24 +0530

    Update TC001_AccountRegistrationTest.java

--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -9,20 +9,5 @@ import org.testng.annotations.BeforeClass;
 
 public class TC001_AccountRegistrationTest {
 
-	public WebDriver driver;
 	
-	@BeforeClass
-	void setup() {
-		driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("");
-		driver.manage().window().maximize();
-	}
-	
-	@AfterClass
-	void teardown() {
-		driver.quit();
-	}
 }
